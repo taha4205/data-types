af7bba6d5838fd50ba303827dc1bc7390737285a
taha <taha68496@gmail.com>
2024-03-02 04:34:21 -0800

    second

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict";
+// data types
+//string
+let a = "hello";
+console.log(a);
+//number
+let b = 10;
+console.log(b);
+//boolean
+let c = true;
+//bigint
+let d = 10n;
+console.log(d);
+//symbol
+let e = Symbol();
+console.log(e);
+//any
+let f = 10;
+f = "aa";
+console.log(f);
+//never
+let g;
+//undefined
+let h = undefined;
+//null
+let i = null;
---
+++ index.ts
@@ -0,0 +1,35 @@
+// data types
+
+//string
+let a: string = "hello";
+console.log(a)
+
+//number
+let b: number = 10;
+console.log(b)
+
+//boolean
+let c: boolean = true;
+console.log(c)
+
+//bigint
+let d: bigint = 10n;
+console.log(d)
+
+//symbol
+let e: symbol = Symbol();
+console.log(e)
+
+//any
+let f: any = 10;
+f = "aa"
+console.log(f)
+
+//never
+let g:never;
+
+//undefined
+let h: undefined = undefined;
+
+//null
+let i: null = null;
